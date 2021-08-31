e11f60d8ff975a81bcdee39862f39d28ac8ae0e3
leorusso2300 <leorusso@gmail.com>
2021-08-31 14:38:58 -0300

    java

---
+++ java/Lista1/src/CalcularExpressao.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CalcularExpressao {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner leia = new Scanner(System.in);
+
+		double a;
+		double b;
+		double c;
+		double d;
+		double r;
+		double s;
+
+		 System.out.print("Número A: \n");
+		 a = leia.nextDouble();
+		 
+		 System.out.print("Número B: \n");
+		 b = leia.nextDouble();
+
+		 System.out.print("Número C: \n");
+		 c = leia.nextDouble();
+		 
+		r = Math.pow((a + b),2.0);
+		s = Math.pow((b + c),2.0);
+		
+		d = (r + s) / 2;
+		
+		System.out.println("R = "+r);
+		System.out.println("\nD = "+d);
+		System.out.println("\nS = "+s);
+		leia.close();
+
+
+	}
+
+}
---
+++ java/Lista1/src/NotaMediaPond.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NotaMediaPond {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+
+		double n1,n2,n3, media, mediapond;
+
+		System.out.println("Nota 1: ");
+		n1 = leia.nextDouble();
+		System.out.println("Nota 2: ");
+		n2 = leia.nextDouble();
+		System.out.println("Nota 3: ");
+		n3 = leia.nextDouble();
+
+		media = (n1 * 2) + (n2*3) + (n3) * 5;
+		mediapond = media/10;
+
+		System.out.println("A média é: " + mediapond);
+		leia.close();
+
+	}
+
+}
