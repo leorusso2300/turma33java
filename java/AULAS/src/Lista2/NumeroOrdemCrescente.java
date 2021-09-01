5784ecdc4aa3d83192bb1237d47805a3241ccf5f
leorusso2300 <leorusso@gmail.com>
2021-09-01 16:19:42 -0300

    Lista 1

--- java/AULAS/src/Lista2/NumeroOrdemCrescente.java
+++ java/AULAS/src/Lista2/NumeroOrdemCrescente.java
@@ -19,20 +19,21 @@ public class NumeroOrdemCrescente {
 		System.out.println("Digite o 3º número: ");
 		n3 = leitor.nextInt();
 
-		if (n1 > n2 && n2 > n3) {
+		if (n1 == n2 && n2 == n3) {
+			System.out.println("Todos os números são iguais");
+		} else if (n1 >= n2 && n2 >= n3) {
 			System.out.println("Os números em ordem crescente ficaram assim: " + n3 + " " + n2 + " " + n1);
-		} else if (n1 > n3 && n3 > n2) {
+		} else if (n1 >= n3 && n3 >= n2) {
 			System.out.println("Os números em ordem crescente ficaram assim: " + n2 + " " + n3 + " " + n1);
-		} else if (n2 > n1 && n1 > n3) {
+		} else if (n2 >= n1 && n1 >= n3) {
 			System.out.println("Os números em ordem crescente ficaram assim: " + n3 + " " + n1 + " " + n2);
-		} else if (n2 > n3 && n3 > n1) {
+		} else if (n2 >= n3 && n3 >= n1) {
 			System.out.println("Os números em ordem crescente ficaram assim: " + n1 + " " + n3 + " " + n2);
-		} else if (n3 > n1 && n1 > n2) {
+		} else if (n3 >= n1 && n1 >= n2) {
 			System.out.println("Os números em ordem crescente ficaram assim: " + n2 + " " + n1 + " " + n3);
-		} else if (n3 > n1 && n2 > n1) {
+		} else if (n3 >= n1 && n2 >= n1) {
 			System.out.println("Os números em ordem crescente ficaram assim: " + n1 + " " + n2 + " " + n3);
 		}
-		
 
 		leitor.close();
 	}
