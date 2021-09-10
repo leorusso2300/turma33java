312971ba0fcc9e82a7656e5712bb16904771ee52
leorusso2300 <leorusso@gmail.com>
2021-09-10 14:52:38 -0300

    Mais exercicios Vetor e Matriz

---
+++ java/Vetor e Matriz/src/Ex1_DigiteCincoValores.java
@@ -0,0 +1,35 @@
+/*
+1) Faça um programa que crie um vetor por leitura com 5
+valores de pontuação de uma atividade e o escreva em seguida.
+Encontre após a maior pontuação e a apresente. 
+*/
+
+
+import java.util.Scanner;
+
+public class Ex1_DigiteCincoValores {
+
+	public static void main(String[] args) {
+
+		double maiorNota;
+		double valores[] = new double[5];
+		int i;
+
+		Scanner read = new Scanner(System.in);
+		maiorNota = 0;
+		i = 0;
+		System.out.println("Digite as notas das Provas:");
+		while (i < 5) {
+			System.out.println("Nota " + (i + 1) + " : ");
+			valores[i] = read.nextDouble();
+			if (valores[i] > maiorNota)
+				maiorNota = valores[i];
+			i++;
+		}
+		
+		System.out.println("A maior nota foi: " + (int) maiorNota);
+		read.close();
+
+	}
+
+}
---
+++ java/Vetor e Matriz/src/Ex3_DuasMatrizes.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+/*
+ 
+Escreve um programa que lê duas matrizes N1 (4,6) e N2(4,6) e cria:
+
+Uma matriz M1 cujos elementos serão as somas dos elementos 
+de mesma posição das matrizes N1 e N2;
+
+Uma matriz M2 cujos elementos serão as diferenças dos elementos 
+de mesma posição das matrizes N1 e N2.
+
+*/
+
+public class Ex3_DuasMatrizes {
+
+	public static void main(String[] args) {
+
+				Scanner read;
+				int N1[][] = new int[4][6];
+				int N2[][] = new int[4][6];
+				int M1[][] = new int[4][6];
+				int M2[][] = new int[4][6];
+				int x;
+				int y;
+				
+				x = 0;
+				y = 0;
+				read = new Scanner(System.in);
+			// input
+				// Ler matriz N1
+				System.out.println("Digite os valores da matriz N1:");
+				
+				while (x < 4)
+				{
+					while (y < 6)
+					{
+						System.out.printf("N1[%d][%d]: ", x, y);
+						N1[x][y] = read.nextInt();
+						y++;
+					}
+					System.out.println();
+					y = 0;
+					x++;
+				}
+				// Ler matriz N2
+					x = 0;
+					y = 0;
+					System.out.println("Digite os valores da matriz N2:");
+					
+					while (x < 4)
+					{
+						while (y < 6)
+						{
+							System.out.printf("N2[%d][%d]: ", x, y);
+							N2[x][y] = read.nextInt();
+							y++;
+						}
+						System.out.println();
+						y = 0;
+						x++;
+					}		
+			// processing e output		
+					// M1
+						x = 0;
+						y = 0;
+						System.out.println("\nM1 (N1 + n1)\n");				
+						while (x < 4)
+						{
+							while (y < 6)
+							{
+								M1[x][y] = N1[x][y] + N2[x][y];
+								System.out.print(M1[x][y] + " ");
+								y++;
+							}
+							System.out.println();
+							y = 0;
+							x++;
+						}
+						System.out.print("\n");
+					// M2
+						x = 0;
+						y = 0;
+						System.out.println("\nM2 (N1 - n1)\n");				
+						while (x < 4)
+						{
+							while (y < 6)
+							{
+								M2[x][y] = N1[x][y] - N2[x][y];
+								System.out.print(M2[x][y] + " ");
+								y++;
+							}
+							System.out.println();
+							y = 0;
+							x++;
+						}
+				read.close();
+		
+	}
+
+}
