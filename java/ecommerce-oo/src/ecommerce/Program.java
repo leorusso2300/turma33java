51596c5d53db1ff83d4f021faf0679b16f8d0e7e
leorusso2300 <leorusso@gmail.com>
2021-09-11 15:21:56 -0300

    Projeto ecommerce POO

---
+++ java/ecommerce-oo/src/ecommerce/Program.java
@@ -0,0 +1,11 @@
+package ecommerce;
+
+public class Program {
+	public static void main(String[] args) {
+		DAL dal = new DAL();
+		System.out.println("aaaa");
+		dal.mostraProdutosDisponiveis();
+		
+	}
+
+}
