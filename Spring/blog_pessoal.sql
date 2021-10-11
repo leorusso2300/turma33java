784f971339e288f3292188ec54c938f286e7a9bb
leorusso2300 <leorusso2607@gmail.com>
2021-10-11 06:41:35 -0300

    Dando uma ajeitada no github

--- Spring/blog_pessoal.sql
+++
@@ -1,4 +0,0 @@
-Insert INTO `db_lojagames`.`tb_categoria` (`tipo_de_consoles`,`tipo_de_jogos`,`descricao`)
- values ('ijrijgirjgi','God of War', 'Jogo muito fijrifjrijg');
- 
-select * from db_lojagames.tb_categoria
